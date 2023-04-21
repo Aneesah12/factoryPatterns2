31e530cedc249be75daded4480590124da031025
aneesahq <aneesah.quraishi@motabilityoperations.co.uk>
2023-04-21 16:27:44 +0100

    Adding abstractFactorySolution for the AbstractFactoryPatternDemo

--- src/main/java/abstractFactoryPatternNetflix/AbstractFactoryPatternDemo.java
+++ src/main/java/abstractFactoryPatternNetflix/AbstractFactoryPatternDemo.java
@@ -1,25 +1,37 @@
 package abstractFactoryPatternNetflix;
 
+import abstractFactoryPatternNetflix.domain.NetflixCatalogue;
+import abstractFactoryPatternNetflix.domain.NetflixDetails;
+import abstractFactoryPatternNetflix.infrastructure.AbstractFactory;
+import abstractFactoryPatternNetflix.infrastructure.FactoryProducer;
+
+import java.util.Collections;
 import java.util.Scanner;
 
 public class AbstractFactoryPatternDemo {
     public static void main(String[] args) {
-        //TODO: Need to find a way to select the factory we want to use/produce
-        //TODO: Need to then get the subclass/genre from that factory
-
         String response;
         String showType;
         String show;
         int rating;
 
         System.out.println("Are you looking to add a Netflix Movie - please answer Yes or No");
-        Scanner myNetflixFactory = new Scanner(System.in);
+        Scanner myNetflixInfo = new Scanner(System.in);
+        response = myNetflixInfo.nextLine();
+
+        AbstractFactory netflixShowFactory = FactoryProducer.getFactory(response);
 
         System.out.println("What is the genre of your show: ROMCOM, ACTION or SCIENCE_FICTION?");
+        showType = myNetflixInfo.nextLine();
 
         System.out.println("What is the name of your show?");
+        show = myNetflixInfo.nextLine();
 
         System.out.println("What rating would you give your show?");
+        rating = Integer.parseInt(myNetflixInfo.nextLine());
 
+        assert netflixShowFactory != null;
+        NetflixCatalogue netflixCatalogue1 = netflixShowFactory.getShowInfo(showType, Collections.singletonList(new NetflixDetails(show, rating)));
+        System.out.println(netflixCatalogue1);
     }
 }
--- src/main/java/abstractFactoryPatternNetflix/infrastructure/FactoryProducer.java
+++ src/main/java/abstractFactoryPatternNetflix/infrastructure/FactoryProducer.java
@@ -1,9 +1,14 @@
 package abstractFactoryPatternNetflix.infrastructure;
 
 public class FactoryProducer {
-    public static AbstractFactory getFactory() {
-
+    public static AbstractFactory getFactory(String response) {
+        if(response.equalsIgnoreCase("Yes")) {
+            return new MovieFactory();
+        } else if (response.equalsIgnoreCase("No")){
+            return new TvShowFactory();
+        } else {
             return null;
+        }
 
     }
 }
