2cf50eb8cb58ec413671f2e3daf9f5e7e7cd021f
aneesahq <aneesah.quraishi@motabilityoperations.co.uk>
2023-04-21 12:52:18 +0100

    Adding updates to abstractFactory challenge

--- src/main/java/abstractFactoryPatternNetflix/AbstractFactoryPatternDemo.java
+++ src/main/java/abstractFactoryPatternNetflix/AbstractFactoryPatternDemo.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 
 public class AbstractFactoryPatternDemo {
     public static void main(String[] args) {
+        //TODO: Need to find a way to select the factory we want to use/produce
+        //TODO: Need to then get the subclass/genre from that factory
+
         String response;
         String showType;
         String show;
         int rating;
 
         System.out.println("Are you looking to add a Netflix Movie - please answer Yes or No");
-        Scanner myNetflixInfo = new Scanner(System.in);
+        Scanner myNetflixFactory = new Scanner(System.in);
 
         System.out.println("What is the genre of your show: ROMCOM, ACTION or SCIENCE_FICTION?");
 
--- src/main/java/abstractFactoryPatternNetflix/infrastructure/MovieFactory.java
+++ src/main/java/abstractFactoryPatternNetflix/infrastructure/MovieFactory.java
@@ -11,13 +11,7 @@ public class MovieFactory extends AbstractFactory {
 
     @Override
     public NetflixCatalogue getShowInfo(String type, List<NetflixDetails> netflixDetails) {
-        if ("ROMCOM".equalsIgnoreCase(type)) {
-            return new RomComMovies(netflixDetails);
-        } else if ("ACTION".equalsIgnoreCase(type)) {
-            return new ActionMovies(netflixDetails);
-        } else if ("SCIENCE_FICTION".equalsIgnoreCase(type)) {
-            return new ScienceFictionMovies(netflixDetails);
-        }
+
         return null;
     }
 }
